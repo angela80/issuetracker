0165b07b2d672f1e18d32c52a5a09159e879216e
angela80 <angeedowling@yahoo.com>
2019-03-17 09:03:58 +0000

    files and database

--- django_issuetracker/settings.py
+++ django_issuetracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'django_issuetracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+ #   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+#}
 
+DATABASES={'default':dj_database_url.parse("postgres://tqdpoqxvpmhzcs:e0aa458d710eb5c518eff82719248e166b3fd97fcb80b34ef6808c25666ef4ee@ec2-54-247-85-251.eu-west-1.compute.amazonaws.com:5432/d3c1ts4k672a4s")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- issuetracker/test_views.py
+++ issuetracker/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
 from .models import Issues
 
 
@@ -25,3 +26,29 @@ class TestViews(TestCase):
     def test_get_edit_page_for_issues_that_does_not_exist(self):
         page = self.client.get("/edit/1")
         self.assertEqual(page.status_code, 404)
+        
+        
+    def test_post_create_an_issues(self):
+        response = self.client.post("/add", {"name": "Create a Test"})
+        issues= get_object_or_404(Issues, pk=1)
+        self.assertEqual(issues.done, False)
+    
+    def test_post_edit_an_issues(self):
+        issues = Issues(name="Create a Test")
+        issues.save()
+        id = issues.id
+
+        response = self.client.post("/edit/{0}".format(id), {"name": "A different name"})
+        issues = get_object_or_404(Issues, pk=id)
+
+        self.assertEqual("A different name", issues.name)
+    
+    def test_toggle_status(self):
+        issues = Issues(name="Create a Test")
+        issues.save()
+        id = issues.id
+
+        response = self.client.post("/toggle/{0}".format(id))
+
+        issues = get_object_or_404(Issues, pk=id)
+        self.assertEqual(issues.done, True)   
